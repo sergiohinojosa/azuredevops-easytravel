dbac5004f0f0c9d04721d68110729ffa940191d3
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-12-29 15:32:55 +0100

    deploy, undeploy and override

--- do_easytravel_deployment_REST.sh
+++ do_easytravel_deployment_REST.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
+# Bash functions to activate and deactivate problempatterns for EasyTravel.
+# Simulate deployments for End2End testing and comparison of releases.
 
-
-# http://easytravel-staging.northeurope.cloudapp.azure.com/easytravel/home
-#DeploymentURI=easytravel-staging.northeurope.cloudapp.azure.com ; EasyTravelDeployment=None; 
-#curl -v "${DEPLOYMENTURI}:8091/services/ConfigurationService/setPluginEnabled?name=${EASYTRAVELDEPLOYMENT}&enabled=true"
-
-EASYTRAVELDEPLOYMENT=$(cat _devopsdemo-easytravel-sre/MyBuildOutputs/easytravel.image)
-
-echo "Read and set deployment: $EASYTRAVELDEPLOYMENT"
-echo "##vso[task.setvariable variable=EASYTRAVELDEPLOYMENT]$EASYTRAVELDEPLOYMENT"
-
-doDeployment() {
-    echo "Doing deployment via REST to ${DEPLOYMENTURI} for image tag:${EASYTRAVELDEPLOYMENT}"
-    curl -v "${DEPLOYMENTURI}:8091/services/ConfigurationService/setPluginEnabled?name=${EASYTRAVELDEPLOYMENT}&enabled=true"
-
+# Read deployment when no arguments
+readDeploymentFromBuild() {
+    EASYTRAVELDEPLOYMENT=$(cat _devopsdemo-easytravel-sre/MyBuildOutputs/easytravel.image)
+    setVsoDeploymentVariable
 }
 
-undoDeployment(){
-    echo "UnDoing deployment via REST to ${DEPLOYMENTURI} for image tag:${EASYTRAVELDEPLOYMENT}"
+setVsoDeploymentVariable() {
+    echo "Read and set deployment: $EASYTRAVELDEPLOYMENT"
+    echo "##vso[task.setvariable variable=EASYTRAVELDEPLOYMENT]$EASYTRAVELDEPLOYMENT"
+}
+
+# If parameters and == undoDeployment then call undoDeployment, else override deployment
+doDeployment() {
+    echo "Doing deployment via REST to '${DEPLOYMENTURI}' for image tag:${EASYTRAVELDEPLOYMENT}"
+    curl -v "${DEPLOYMENTURI}:8091/services/ConfigurationService/setPluginEnabled?name=${EASYTRAVELDEPLOYMENT}&enabled=true"
+}
+
+undoDeployment() {
+    echo "UnDoing deployment via REST to '${DEPLOYMENTURI}' for image tag:${EASYTRAVELDEPLOYMENT}"
     curl -v "${DEPLOYMENTURI}:8091/services/ConfigurationService/setPluginEnabled?name=${EASYTRAVELDEPLOYMENT}&enabled=false"
 }
 
 # If calling with a parameter undo deployment
-if [[ $# -eq 1 ]]; then
-
+if [[ $# -eq 0 ]]; then
+    readDeploymentFromBuild
+    doDeployment
+elif [[ $1 == "undeploy" ]]; then
+    # We undo the deployment. EASYTRAVELDEPLOYMENT should have been setted before either 
+    # overriding or reading from image
     undoDeployment
 else
-    doDeployment
+    # Override deployment
+    EASYTRAVELDEPLOYMENT=$1
+    # Set it on the Build
+    setVsoDeploymentVariable
+    # Activate it over REST
+    doDeployment 
 fi
